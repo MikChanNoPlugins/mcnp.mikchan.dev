8c361001a31456e83721f044c6594a337e8e00ad
George Endo <georgeendo@hotmail.com>
2022-04-28 19:47:04 +0300

    Dist

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
     transpileDependencies: true,
-
+    outputDir: "docs",
     pluginOptions: {
-      vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
-    }
+        vuetify: {
+            // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+        },
+    },
 });
