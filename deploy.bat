b2b0f7684a5f395cab7f4a57fa2ee226d15d6c9d
George Endo <georgeendo@hotmail.com>
2022-04-28 20:07:37 +0300

    Add deploy

---
+++ deploy.bat
@@ -0,0 +1,10 @@
+yarn build
+
+cd dist
+
+git init
+git add -A
+git commit -m Deploy
+git push -f https://github.com/MikChanNoPlugins/mikchannoplugins.github.io.git master:gh-pages
+
+cd ..
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
     transpileDependencies: true,
-    outputDir: "docs",
     pluginOptions: {
         vuetify: {
             // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
