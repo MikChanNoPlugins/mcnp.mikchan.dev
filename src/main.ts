7b2773f39d883ed697ed5c1b962b2b96f15dc9c6
wtlgo <georgeendo0@gmail.com>
2022-08-01 02:47:21 +0300

    Recreate the project from scratch

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,5 @@
 import { createApp } from "vue";
+import "./style.css";
 import App from "./App.vue";
-import router from "./router";
-import vuetify from "./plugins/vuetify";
-import i18n from "./plugins/i18n";
-import { loadFonts } from "./plugins/webfontloader";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-loadFonts();
-
-createApp(App)
-    .component("font-awesome-icon", FontAwesomeIcon)
-    .use(i18n)
-    .use(router)
-    .use(vuetify)
-    .mount("#app");
+createApp(App).mount("#app");
