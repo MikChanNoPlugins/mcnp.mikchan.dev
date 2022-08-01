a0bc62846897067455b25fc913d6f08b62471e68
wtlgo <georgeendo0@gmail.com>
2022-08-01 18:51:13 +0300

    Add temporary plugins view

--- src/views/PluginsView.vue
+++ src/views/PluginsView.vue
@@ -11,11 +11,27 @@
                 </v-card>
             </v-col>
         </v-row>
+
+        <v-row>
+            <v-col
+                v-for="(item, idx) in longArray"
+                cols="4"
+                :key="`card_${idx}`"
+            >
+                <v-card>
+                    <v-card-title> Test Title {{ item }} </v-card-title>
+                    <v-card-text> Test Body {{ item }} </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 
 <script setup lang="ts">
+import { reactive } from "vue";
 import { useI18n } from "../plugins/i18n";
+
+const longArray = reactive([...new Array(50)].map((_, idx) => idx + 1));
 
 const { t } = useI18n();
 </script>
