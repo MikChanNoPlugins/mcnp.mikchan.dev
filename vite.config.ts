3f69909054e108b99cc0a15ff6fad14f176cc2a4
wtlgo <georgeendo0@gmail.com>
2022-08-01 05:47:36 +0300

    Add icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { vueI18n } from "@intlify/vite-plugin-vue-i18n";
+import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
+import { fileURLToPath, URL } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +12,11 @@ export default defineConfig({
         vueI18n({
             include: path.resolve(__dirname, "./path/to/src/locales/**"),
         }),
+        VitePWA({ registerType: "autoUpdate" }),
     ],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
 });
