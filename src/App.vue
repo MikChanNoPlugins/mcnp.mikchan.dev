a002013f3e4ef10f5b4aa45e00aefb31c76a81fc
wtlgo <georgeendo0@gmail.com>
2022-08-01 12:15:04 +0300

    Add another fance background for the light theme. Src: https://pixabay.com/photos/clouds-dark-storm-background-2725520/

--- src/App.vue
+++ src/App.vue
@@ -12,10 +12,14 @@
 <script setup lang="ts">
 import { computed, provide, ref } from "vue";
 import AppBar from "./components/AppBar.vue";
-import background from "@/assets/background.jpg";
+import backgroundDark from "@/assets/background-dark.jpg";
+import backgroundLight from "@/assets/background-light.jpg";
 
 const isDarkTheme = ref(sessionStorage.getItem("is_dark_theme") !== "0");
 const theme = computed(() => (isDarkTheme.value ? "dark" : "light"));
+const background = computed(() =>
+    isDarkTheme.value ? backgroundDark : backgroundLight
+);
 function switchTheme(val: boolean) {
     isDarkTheme.value = val;
     sessionStorage.setItem("is_dark_theme", isDarkTheme.value ? "1" : "0");
