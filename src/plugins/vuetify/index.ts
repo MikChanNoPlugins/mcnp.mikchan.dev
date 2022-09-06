c75920e48b7f79ab5136ed3ea81210797bfcd4bb
wtlgo <georgeendo0@gmail.com>
2022-09-06 11:33:30 +0300

    Add only the specific icon

--- src/plugins/vuetify/index.ts
+++ src/plugins/vuetify/index.ts
@@ -3,14 +3,14 @@ import { createVuetify } from "vuetify";
 import { createVueI18nAdapter } from "vuetify/locale/adapters/vue-i18n";
 import { i18n, useI18n } from "../i18n";
 import { aliases, mdi } from "vuetify/lib/iconsets/mdi-svg";
+
 import { fa } from "vuetify/lib/iconsets/fa-svg";
-
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-import { fab } from "@fortawesome/free-brands-svg-icons";
+// import { fas } from "@fortawesome/free-solid-svg-icons";
+import { faVk } from "@fortawesome/free-brands-svg-icons";
 
-library.add(fas);
-library.add(fab);
+// library.add(fas);
+library.add({ faVk });
 
 const vuetify = createVuetify({
     locale: createVueI18nAdapter({
