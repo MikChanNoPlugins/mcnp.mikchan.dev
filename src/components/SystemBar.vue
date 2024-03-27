08e9f1776e6a8f2ba99343aba4dfee99e248d626
wtlgo <georgeendo0@gmail.com>
2024-03-27 20:34:10 +0300

    Remove anime quotes

--- src/components/SystemBar.vue
+++ src/components/SystemBar.vue
@@ -13,16 +13,14 @@
 
 <template>
     <v-system-bar>
-        <span class="quote"> {{ quote }} </span>
+        <span class="quote"> </span>
         <v-spacer />
         <span class="time"> {{ time }} </span>
     </v-system-bar>
 </template>
 
 <script setup lang="ts">
-import { useAnimeQuote } from "../hooks/useAnimeQuote";
 import { useTime } from "../hooks/useTime";
 
-const { quote } = useAnimeQuote();
 const { time } = useTime();
 </script>
